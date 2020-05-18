585a73976a813d8dba2b867261e38b3bf0eee989
Cole <coletaylor017@gmail.com>
2020-05-17 23:37:21 -0600

    Initial commit

---
+++ src/Project.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+export default class Project extends Component {
+    render() {
+        return <div className="col-md-6 mb-4">
+            <div className={"card " + (this.props.dark ? "text-light bg-dark" : null)}>
+                <div className="card-header">
+                    <h4>{this.props.title}</h4>
+                    <h6>{this.props.subtitle}</h6>
+                </div>
+                <div className="card-body">
+
+                    <p>{this.props.body}</p>
+
+                    {this.props.buttons.map(function (button) {
+                        return <a className="btn btn-primary mr-1" href={button.link} target="_blank">{button.text}</a>
+                    })}
+
+                    <hr />
+
+                    <a href="http://ainotebook.herokuapp.com" target="_blank">
+                        <img className="img-fluid" src={this.props.imageSrc} alt="" />
+                    </a>
+                </div>
+            </div>
+        </div >
+    }
+}
