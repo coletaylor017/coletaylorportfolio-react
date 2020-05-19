c846422fff88a3245825b2b3b17441fc39ae1632
Cole <coletaylor017@gmail.com>
2020-05-19 01:24:27 -0600

    Final (hopefully) fix to HTTPS redirect

--- app.js
+++ app.js
@@ -4,19 +4,26 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-// app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, 'build')));
+
+// https://stackoverflow.com/a/23894573/5166231
+const env = process.env.NODE_ENV || 'development';
+
+var forceSsl = function (req, res, next) {
+    if (req.headers['x-forwarded-proto'] !== 'https') {
+        return res.redirect(['https://', req.get('Host'), req.url].join(''));
+    }
+    return next();
+};
+
+if (env === 'production') {
+    app.use(forceSsl);
+}
 
 app.get('/', function (req, res) {
-    console.log("request!");
-    // https://stackoverflow.com/a/7458587/5166231
-    if (!req.secure) {
-        console.log("not secure!");
-        res.redirect('https://' + req.headers.host + req.url);
-    }
-    else {
-        res.sendFile(path.join(__dirname, 'build', 'index.html'));
-    }
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
+
 app.listen(process.env.PORT, function () {
     console.log("Server started on port " + process.env.PORT);
 });
