ed4c286f6c03c26fb704c1b80aff6837c8f8a5bb
Cole <coletaylor017@gmail.com>
2020-05-19 01:13:44 -0600

    Try to fix HTTPS redirect again

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
-// All copied from the Create-React-App docs at:
+// Mostly copied from the Create-React-App docs at:
 // https://create-react-app.dev/docs/deployment/
 const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'build')));
+// app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('/', function (req, res) {
+    console.log("request!");
+    // https://stackoverflow.com/a/7458587/5166231
     if (!req.secure) {
+        console.log("not secure!");
         res.redirect('https://' + req.headers.host + req.url);
     }
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+    else {
+        res.sendFile(path.join(__dirname, 'build', 'index.html'));
+    }
 });
 app.listen(process.env.PORT, function () {
     console.log("Server started on port " + process.env.PORT);
